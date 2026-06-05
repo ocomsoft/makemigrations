e3ff1ea31190060b54cdd9f39b8472f45f2e0858
Scott Warren <scottwarren@ocom.com.au>
2026-06-05 11:24:48 +1000

    Bump version: 1.8.5 → 1.8.6

--- internal/version/version.go
+++ internal/version/version.go
@@ -32,7 +32,7 @@ import (
 var (
 	// Version represents the current morphic version
 	// This variable is updated by bumpversion during releases or set via ldflags
-	Version = "1.8.5"
+	Version = "1.8.6"
 
 	// BuildDate is set via ldflags during build
 	BuildDate = "unknown"
